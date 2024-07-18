bd048584acf3c7e9cb6f05959a77e3ec045e491b
CHIRAG DALMIA <119760552+ChiragDalmia@users.noreply.github.com>
2024-07-17 23:23:51 -0400

    added header and footer components

---
+++ angular-classswork/A3dalmiac/src/app/footer-dalmiac/footer-dalmiac.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-dalmiac',
+  templateUrl: './footer-dalmiac.component.html',
+  styleUrl: './footer-dalmiac.component.css'
+})
+export class FooterDalmiacComponent {
+
+}
---
+++ angular-classswork/A3dalmiac/src/app/header-dalmiac/header-dalmiac.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-dalmiac',
+  templateUrl: './header-dalmiac.component.html',
+  styleUrl: './header-dalmiac.component.css'
+})
+export class HeaderDalmiacComponent {
+
+}
